b3fbec3f6838413c9081467ace316cb0e6bfd3fa
Jack <jarbus@tutanota.com>
2022-04-29 11:42:11 -0400

    Add communications

--- tradeenv.py
+++ tradeenv.py
@@ -12,21 +12,22 @@ class Trade(MultiAgentEnv):
     action_space = Discrete(2)
 
     def __init__(self, env_config):
-        food_types = env_config.get("food_types", 2)
+        self.food_types = env_config.get("food_types", 2)
         num_agents = env_config.get("num_agents", 2)
-        episode_length = env_config.get("episode_length", 100)
+        self.max_steps = env_config.get("episode_length", 100)
+        self.vocab_size = env_config.get("vocab_size", 6)
         super().__init__()
-        self.food_types = food_types
-        self.num_inputs = food_types * (num_agents+1)
+        self.num_inputs = self.food_types * (num_agents+1) + (self.vocab_size*num_agents)
         self.agent_food_counts = dict()
-        self.max_steps = episode_length
         self.MOVES = []
-        for f in range(food_types):
+        for f in range(self.food_types):
             self.MOVES.extend([f"PICK_{f}", f"PLACE_{f}"])
+        self.MOVES.extend([f"COMM_{c}" for c in range(self.vocab_size)])
         self.MOVES.append("NONE")
         self.num_actions = len(self.MOVES)
         self.num_exchanges = []
         self.exchange_table = []
+        self.communications = {}
 
         self.possible_agents = ["player_" + str(r) for r in range(num_agents)]
         self.agents = self.possible_agents[:]
@@ -45,6 +46,7 @@ class Trade(MultiAgentEnv):
         self.rewards = {agent: 0 for agent in self.agents}
         self.table = [0]*self.food_types
         self.steps = 0
+        self.communications = {agent: [0 for j in range(self.vocab_size)] for agent in self.agents}
         self.num_exchanges = [0]*self.food_types
         self.exchange_table = [defaultdict(int) for f in range(self.food_types)]
         self.agent_food_counts = dict()
@@ -62,15 +64,20 @@ class Trade(MultiAgentEnv):
             print(f"{agent}: {self.agent_food_counts[agent]} {self.compute_done(agent)}")
         print(f"table: {self.table}")
         print(f"Total exchanged so far: {self.num_exchanges}")
+        for agent, comm in self.communications:
+            if max(comm) >= 1:
+                print(f"{agent} said {comm.index(1)}")
 
     def compute_observation(self, agent=None):
         # stores action of current agent
         curr_state = self.table.copy()
         if agent:
             curr_state.extend([*self.agent_food_counts[agent]])
+            curr_state.extend([*self.communications[agent]])
         for other_agent in self.agents:
             if agent != other_agent:
                 curr_state.extend(self.agent_food_counts[other_agent])
+                curr_state.extend([*self.communications[other_agent]])
         curr_state = [s / 10 for s in curr_state]
         return curr_state
 
@@ -89,8 +96,9 @@ class Trade(MultiAgentEnv):
 
     def step(self, actions):
         # all agents execute their action
+        self.communications = {agent: [0 for j in range(self.vocab_size)] for agent in self.agents}
         for (agent, action) in actions.items():
-            if action != self.num_actions - 1:
+            if action in range(self.food_types * 2):
                 pick = (action % 2 == 0)
                 food = floor(action / 2)
                 if pick:
@@ -100,10 +108,12 @@ class Trade(MultiAgentEnv):
                     self.exchange_table[food] = defaultdict(int)
                 elif self.agent_food_counts[agent][food] >= PLACE_AMOUNT:
                     self.agent_food_counts[agent][food] -= PLACE_AMOUNT
-                    if self.agent_food_counts[agent][food] < 0:
-                        print(self.agent_food_counts)
                     self.table[food] += PLACE_AMOUNT
                     self.exchange_table[food][agent] += PLACE_AMOUNT
+            elif action in range(self.food_types * 2, self.num_actions-1): # last action is noop
+                symbol = action - self.food_types * 2
+                assert symbol in range(self.vocab_size)
+                self.communications[agent][symbol] = 1
 
         for agent in self.agents:
             self.agent_food_counts[agent] = [x - 0.1 for x in self.agent_food_counts[agent]]
@@ -117,6 +127,26 @@ class Trade(MultiAgentEnv):
         dones = {**dones, "__all__": all(dones.values())}
         infos = {}
         return obs, rewards, dones, infos
+
+class TradeCallback(DefaultCallbacks):
+
+    def on_episode_start(self, worker, base_env, policies, episode, **kwargs):
+        self.comm_history = [0 for i in range(base_env.get_unwrapped()[0].vocab_size)]
+
+    def on_episode_step(self, worker, base_env, policies, episode, **kwargs):
+        # there is a bug where on_episode_step gets called where it shouldn't
+        env = base_env.get_unwrapped()[0]
+        for agent, comm in env.communications.items():
+            if max(comm) == 1:
+                symbol = comm.index(1)
+                self.comm_history[symbol] += 1
+
+    def on_episode_end(self, worker, base_env, policies, episode, **kwargs,):
+        env = base_env.get_unwrapped()[0]
+        for food, count in enumerate(env.num_exchanges):
+            episode.custom_metrics[f"exchange_{food}"] = count
+        for symbol, count in enumerate(self.comm_history):
+            episode.custom_metrics[f"comm_{symbol}"] = count
 
 
 if __name__ == "__main__":
@@ -133,17 +163,3 @@ if __name__ == "__main__":
         if dones["__all__"]:
             print("game over")
             break
-
-class TradeCallback(DefaultCallbacks):
-
-    def on_episode_end(
-        self,
-        worker,  #: RolloutWorker,
-        base_env,  #: BaseEnv,
-        policies,  #: Dict[str, Policy],
-        episode,  #: MultiAgentEpisode,
-        **kwargs,
-    ):
-        num_exchanges = base_env.get_unwrapped()[0].num_exchanges
-        for food, count in enumerate(num_exchanges):
-            episode.custom_metrics[f"exchange_{food}"] = count
