6c6d90996e1d11327d75c36fcd165cdb9b1e862e
Jack <jarbus@tutanota.com>
2022-06-08 18:35:34 -0400

    Adds resume check to final-check.sh

--- final-check.sh
+++ final-check.sh
@@ -1,1 +1,2 @@
-grep -oP "\s*\K(num_agents = |timesteps|samples|freq|batch_size =|dist_coeff|lr|local_dir|name=).*$" tune.py
+grep -oP "\s*\K(num_agents = |resume|timesteps|samples|freq|batch_size =|dist_coeff|lr|local_dir|name=).*$" tune.py
+cat slurm.sh
