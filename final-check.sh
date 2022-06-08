c8449cee08253af4c6875ec3d2d4bd059e51f179
Jack <jarbus@tutanota.com>
2022-06-07 23:48:56 -0400

    Update final check

--- final-check.sh
+++ final-check.sh
@@ -1,1 +1,1 @@
-grep -oP "\s*\K(num_agents = |timesteps|samples|freq|batch_size =|dist_coeff|lr).*$" tune.py
+grep -oP "\s*\K(num_agents = |timesteps|samples|freq|batch_size =|dist_coeff|lr|local_dir|name=).*$" tune.py
