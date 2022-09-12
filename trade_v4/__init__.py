054dc727e5110a37d1c1a88692edcc96e11ddc0a
Jack <jarbus@tutanota.com>
2022-09-12 16:17:06 -0400

    Remove Population from imports

--- trade_v4/__init__.py
+++ trade_v4/__init__.py
@@ -1,1 +1,1 @@
-from .trade_v4 import Trade, METABOLISM, PLACE_AMOUNT, TradeCallback, POLICY_MAPPING_FN, FireCornerSpawner, FoodSpawner, Population
+from .trade_v4 import Trade, METABOLISM, PLACE_AMOUNT, TradeCallback, POLICY_MAPPING_FN, FireCornerSpawner, FoodSpawner
--- trade_v4/trade_v4/__init__.py
+++ trade_v4/trade_v4/__init__.py
@@ -1,4 +1,4 @@
 from .trade_env import Trade, METABOLISM, PLACE_AMOUNT
 from .trade_callback import TradeCallback
-from .utils import POLICY_MAPPING_FN, Population
+from .utils import POLICY_MAPPING_FN
 from .spawners import FireCornerSpawner, FoodSpawner 
