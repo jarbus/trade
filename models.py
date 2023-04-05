c87ee2b35f190feda20110dfe4dd1c376e6101a9
jarbus <jarbus@tutanota.com>
2023-04-05 11:22:29 -0400

    add: largeone model config

--- models.py
+++ models.py
@@ -10,6 +10,17 @@ extrasmall = {
     "max_seq_len": 50,
 }
 
+largeone = {
+    "conv_filters": [[128, [1, 1], 1], [128, [1, 1], 1], [128, [1, 1], 1]],
+    "conv_activation": "relu",
+    "post_fcnet_hiddens": [256, 256],
+    "post_fcnet_activation": "relu",
+    "use_lstm": True,
+    "lstm_cell_size": 512,
+    "lstm_use_prev_action": False,
+    "max_seq_len": 50,
+}
+
 small = {
     "conv_filters": [[32, [3, 3], 1], [32, [3, 3], 1]],
     "conv_activation": "relu",
